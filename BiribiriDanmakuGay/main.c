efe52b6e7a76b55b07b03b6f0a220ecf0ffe055c
Li Keqing <me@kaze.ai>
2022-02-16 01:01:39 +0800

    Yes?

--- BiribiriDanmakuGay/main.c
+++ BiribiriDanmakuGay/main.c
@@ -3,11 +3,33 @@
 #include "resource.h"
 
 extern IMAGE_DOS_HEADER __ImageBase;
+HINSTANCE g_hInstance = &__ImageBase;
+
+typedef struct _USERDATA
+{
+    WSAOVERLAPPED overlapped;
+    HANDLE hIocp;
+    HANDLE hTcpSock;
+    BOOL bRunning;
+
+} USERDATA;
 
 static INT_PTR CALLBACK MainDialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
+    case WM_INITDIALOG:
+    {
+        USERDATA* userData = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(USERDATA));
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, userData);
+        return TRUE;
+    }
+    case WM_DESTROY:
+    {
+        USERDATA* userData = GetWindowLongPtr(hWnd, GWLP_USERDATA);
+        HeapFree(GetProcessHeap(), 0, userData);
+        return TRUE;
+    }
     case WM_CLOSE:
     {
         EndDialog(hWnd, 0);
@@ -15,8 +37,13 @@ static INT_PTR CALLBACK MainDialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPAR
     }
     case WM_COMMAND:
     {
-        switch (LOWORD(IDC_BUTTON_ACTION))
+        switch (LOWORD(wParam))
         {
+        case IDC_BUTTON_ACTION:
+        {
+            SendMessage(hWnd, WM_SETTEXT, 0, "弹幕基");
+            return TRUE;
+        }
         default:
             break;
         }
@@ -31,7 +58,7 @@ int WINAPI WinMain(HINSTANCE arg0, HINSTANCE arg1, LPSTR arg2, int arg3)
 {
     WSADATA wsa;
     WSAStartup(MAKEWORD(2, 2), &wsa);
-    DialogBox(GetInstance(), MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, MainDialogProc);
+    DialogBox(g_hInstance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, MainDialogProc);
     WSACleanup();
     return 0;
 }
